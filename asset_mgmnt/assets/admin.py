826e25c4d9242c848304556029c8633f0fdce50c
mukiraz <mukiraz@gmail.com>
2024-05-31 12:57:42 +0200

    asset type added

--- asset_mgmnt/assets/admin.py
+++ asset_mgmnt/assets/admin.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Category,AssetType,Currency, UnitType, SwissStockHTMLContent
+from .models import Category,Currency, UnitType, SwissStockHTMLContent
 
 # Register your models here.
 
 admin.site.register(Category)
-admin.site.register(AssetType)
 admin.site.register(Currency)
 admin.site.register(UnitType)
 admin.site.register(SwissStockHTMLContent)
