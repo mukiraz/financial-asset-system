ccc4e216eee4b29e9dc33290ed4149c348a4c81b
mukiraz <mukiraz@gmail.com>
2024-05-31 12:58:47 +0200

    asset type added

--- asset_mgmnt/assets/models.py
+++ asset_mgmnt/assets/models.py
@@ -36,4 +36,15 @@ class SwissStockHTMLContent(models.Model):
     html_content = models.TextField()
 
     def __str__(self):
-        return str(self.pk)   
+        return str(self.pk)
+    
+class AssetType(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    current_price = models.FloatField() 
+    description = models.TextField(null=True, blank=True)
+    created_at  = models.DateTimeField(auto_now_add = True) 
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return f"{self.name}"
