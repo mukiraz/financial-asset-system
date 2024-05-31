976ca4abca45c38fdc47eadb6bd3b358f282fec4
mukiraz <mukiraz@gmail.com>
2024-05-31 12:02:33 +0200

    asset type created, Category, Currency, Swiss html, unit type initial values added to database

--- asset_mgmnt/assets/admin.py
+++ asset_mgmnt/assets/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category,AssetType,Currency, UnitType, SwissStockHTMLContent
+
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(AssetType)
+admin.site.register(Currency)
+admin.site.register(UnitType)
+admin.site.register(SwissStockHTMLContent)
--- asset_mgmnt/assets/models.py
+++ asset_mgmnt/assets/models.py
@@ -37,3 +37,14 @@ class SwissStockHTMLContent(models.Model):
 
     def __str__(self):
         return str(self.pk)
+    
+class AssetType(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    current_price = models.FloatField() 
+    description = models.TextField(null=True, blank=True)
+    created_at  = models.DateTimeField(auto_now_add = True) 
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return f"{self.name}"
