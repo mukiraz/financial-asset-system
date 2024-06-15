d624250610b9bdc37507cade9e4ccf923a72835b
mukiraz <mukiraz@gmail.com>
2024-06-15 21:59:14 +0200

    404 page creted

--- asset_mgmnt/asset_mgmnt/urls.py
+++ asset_mgmnt/asset_mgmnt/urls.py
@@ -14,10 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import handler404
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("assets.urls"))
 ]
+
+def custom_page_not_found_view(request, exception):
+    return render(request, "404.html", {})
+
+handler404 = custom_page_not_found_view
